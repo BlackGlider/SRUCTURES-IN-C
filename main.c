96573af6cde1e5078600fe7b4455c37519e3bfe5
Glo Alba <glomontalba7@gmail.com>
2018-01-08 19:30:10 +0100

    Array of Structure
    Array of STructure

--- main.c
+++ main.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-struct dati_patente
-{
-    int patente_id;
-    char nome_patente[50];
+struct data{
+    char name[30];
+    char surname[30];
+    int age;
 };
-
-struct dati_personali
-{
-    int id;
-    char name[20];
-    float punteggio;
-    //structure within structure
-    struct dati_patente da_pat;
-} dati_utente;
 
 int main(){
     
-    struct dati_personali dati_utente = {29, "Gino", 79, 345678998, "Car-Destroy"};
+    int i;
+    struct data account[2];
     
-    printf("Id is: %d\n", dati_utente.id);
-    printf("Name is: %s\n", dati_utente.name);
-    printf("Points are: %f\n", dati_utente.punteggio);
-    printf("Patente ID: %d\n", dati_utente.da_pat.patente_id);
-    printf("Nome Patente: %s\n", dati_utente.da_pat.nome_patente);
+    //1st account's data
+    strcpy(account[0].name, "Joseph");
+    strcpy(account[0].surname, "Jackson");
+    account[0].age = 19;
+    
+    //2nd account's data
+    strcpy(account[1].name, "Marcus");
+    strcpy(account[1].surname, "Monte");
+    account[1].age = 20;
+    
+    //3rd account's data
+    strcpy(account[2].name, "Bob");
+    strcpy(account[2].surname, "Matter");
+    account[2].age = 23;
+    
+    for(i=0; i<3; i++)
+    {
+        printf("User's Data Accounts: %d\n", i+1);
+        printf("Name: %s\n", account[i].name);
+        printf("Surname: %s\n", account[i].surname);
+        printf("Age: %d\n", account[i].age);
+    }
     
     return 0;
 }

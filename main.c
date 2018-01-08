a025f30091cafcdd709999c0831c4d21e09b9157
Glo Alba <glomontalba7@gmail.com>
2018-01-08 16:45:59 +0100

    Leggi e Scrivi
    Leggi e SCrivi

--- main.c
+++ main.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
 #define STR_LEN 30
 
-struct {
+typedef struct {
     char name[STR_LEN+1];
     char surname[STR_LEN+1];
     int age;
-} utente1, utente2;
+} User;
 
+void print_data(User u);
 
 int main(){
-  
+    
+    User user1;
+    
     printf("Inserire dati utente 1: ");
-    printf("Name\n");
-    scanf("%s", utente1.name);
-    printf("Surname\n");
-    scanf("%s", utente1.surname);
-    printf("Age\n");
-    scanf("%d", &utente1.age);
+    printf("\n\n");
+    printf("Name: \n");
+    scanf("%s", user1.name);
+    printf("Surname: \n");
+    scanf("%s", user1.surname);
+    printf("Age: \n");
+    scanf("%d", &user1.age);
     
+    printf("\n\n");
     
+    print_data(user1);
     
+    printf("\n\n");
     
     return 0;
+}
+
+void print_data(User u){
+    
+    printf("Name: %s\n", u.name);
+    printf("Name: %s\n", u.surname);
+    printf("Name: %d\n", u.age);
+    
 }
